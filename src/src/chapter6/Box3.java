be41381c8c7afcbb867c4311f0ea7efb4dc76c6f
Tigran Drmeyan <drmeyant@gmail.com>
2021-11-02 23:10:18 +0400

    all

--- src/src/chapter6/Box3.java
+++ src/src/chapter6/Box3.java
@@ -5,7 +5,7 @@ public class Box3 {
     double height;
     double depth;
     void  volume() {
-        System.out.println("obyom raven");
+        System.out.print("obyom raven");
         System.out.println(width*height*depth);
     }
 }
