10421e5d232ea091ab7ca2c70588cbc0508746c6
Tigran Drmeyan <drmeyant@gmail.com>
2021-11-19 19:15:15 +0400

    authorCLASSWORK

---
+++ src/src/authorclasswork/Author.java
@@ -0,0 +1,76 @@
+package src.authorclasswork;
+
+public class Author {
+
+    private String name;
+    private String surname;
+    private int age;
+    private String email;
+    private String gender;
+
+    public Author() {
+    }
+
+    public Author(String name, String surname, int age, String email, String gender) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.email = email;
+        this.gender = gender;
+
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+
+}
---
+++ src/src/authorclasswork/AuthorTest.java
@@ -0,0 +1,87 @@
+package src.authorclasswork;
+
+import java.util.Scanner;
+
+public class AuthorTest {
+
+    static Scanner scanner = new Scanner(System.in);
+    static AuthorStorage authorStorage = new AuthorStorage();
+
+    private static final String EXIT = "0";
+    private static final String ADD_AUTHOR = "1";
+    private static final String SEARCH_AUTHORS = "2";
+    private static final String SEARCH_AUTHORS_BY_AGE = "3";
+    private static final String PRINT_AUTHORS = "4";
+
+
+    public static void main(String[] args) {
+        authorStorage.add(new Author("poxos", "poxosyan",
+                22, "poxos@mail.com", "male"));
+        authorStorage.add(new Author("poxosuhi", "poxosyan",
+                23, "poxosuhi@mail.com", "female"));
+        authorStorage.add(new Author("petros", "petrosyan",
+                25, "petros@mail.com", "male"));
+        boolean isRun = true;
+        while (isRun) {
+            printCommands();
+            String command = scanner.nextLine();
+            switch (command) {
+                case EXIT:
+                    isRun = false;
+                    break;
+                case ADD_AUTHOR:
+                    addAuthor();
+                    break;
+                case SEARCH_AUTHORS:
+                    searchByName();
+                    break;
+                case SEARCH_AUTHORS_BY_AGE:
+                    searchByAge();
+                case PRINT_AUTHORS:
+                    authorStorage.print();
+                    break;
+                default:
+                    System.out.println("Invalid command");
+            }
+        }
+    }
+
+    private static void searchByAge() {
+        System.out.println("please input min age");
+        int minAge = Integer.parseInt(scanner.nextLine());
+        System.out.println("please input max age");
+        int maxAge = Integer.parseInt(scanner.nextLine());
+        authorStorage.searchByAge(minAge, maxAge);
+    }
+
+    private static void printCommands() {
+        System.out.println("please input " + EXIT + " for EXIT");
+        System.out.println("please input " + ADD_AUTHOR + " for author");
+        System.out.println("please input " + SEARCH_AUTHORS + " for search author by name");
+        System.out.println("please input " + SEARCH_AUTHORS_BY_AGE + "for search authors by age");
+        System.out.println("please input " + PRINT_AUTHORS + " for print authors");
+    }
+
+    private static void searchByName() {
+        System.out.println("please input keyword");
+        String keyword = scanner.nextLine();
+        authorStorage.searchByName(keyword);
+    }
+
+    private static void addAuthor() {
+        System.out.println("please input authors name");
+        String name = scanner.nextLine();
+        System.out.println("please input authors surname");
+        String surname = scanner.nextLine();
+        System.out.println("please input authors email");
+        String email = scanner.nextLine();
+        System.out.println("please input authors gender");
+        String gender = scanner.nextLine();
+        System.out.println("please input authors age");
+        int age = Integer.parseInt(scanner.nextLine());
+
+        Author author = new Author(name, surname, age, email, gender);
+        authorStorage.add(author);
+        System.out.println("Thank you, author was added");
+    }
+}
